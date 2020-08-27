1fb6b8db3f020bd03cc73b38c4f14fcc3b80cccc
Allan David <allanbrados@gmail.com>
2020-08-26 23:06:49 -0300

    first try

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,22 @@ fn divide_vector(image::Rgb([a1, a2, a3]) : image::Rgb<u8>, factor : u8) -> imag
     [a1.wrapping_div(factor), a2.wrapping_div(factor), a3.wrapping_div(factor)].into()
 }
 
-fn mult_vector(image::Rgb([a1, a2, a3]) : image::Rgb<u8>, factor : u8) -> image::Rgb<u8> {
-    [a1.wrapping_mul(factor), a2.wrapping_mul(factor), a3.wrapping_mul(factor)].into()
+fn mult_vector(image::Rgb([a1, a2, a3]) : image::Rgb<u8>, factor : f32) -> image::Rgb<u8> {
+    let na1 = (a1 as f32 * factor) as u8;
+    let na2 = (a2 as f32 * factor) as u8;
+    let na3 = (a3 as f32 * factor) as u8;
+
+    println!("{} - {} - {}", na1, na2, na3);
+
+    [na1, na2, na3].into()
 }
 
 fn array_to_u32_array(image::Rgb([a1, a2, a3]) : image::Rgb<u8>) -> [u32; 3] {
     [a1.into(), a2.into(), a3.into()]
 }
 
-fn sum_axes_of_vector([a1, a2, a3] : [u32; 3]) -> u32 {
-    a1 + a2 + a3
+fn length([a1, a2, a3] : [u32; 3]) -> u32 {
+    ((a1.pow(2) + a2.pow(2) + a3.pow(2)) as f32).sqrt() as u32
 }
 
 fn main() {
@@ -39,26 +45,31 @@ fn main() {
     let mut new_img = image::ImageBuffer::new(img.dimensions().0, img.dimensions().1);
 
     for (x, y, pixel) in img.enumerate_pixels() {
-        let pixel_color = image::Pixel::to_rgb(pixel);
+        let gray_color = image::Pixel::to_rgb(pixel);
 
         let chosen_color : image::Rgb<u8> = pallete_colors.iter().fold(*pallete_colors.first().unwrap(), |x, y|{
-            let dist_x = sum_axes_of_vector(array_to_u32_array(subtract_vector(pixel_color, x)));
-            let dist_y = sum_axes_of_vector(array_to_u32_array(subtract_vector(pixel_color, *y)));
+            let dist_x = length(array_to_u32_array(subtract_vector(gray_color, x)));
+            let dist_y = length(array_to_u32_array(subtract_vector(gray_color, *y)));
 
-            if dist_x > dist_y { x } else { *y }
+            // println!("distx {} disty {}", dist_x, dist_y);
+
+            if dist_x <= dist_y { x } else { *y }
         });
 
-        let gray = mult_vector(divide_vector(pixel_color, 10), 2);
-        let color = mult_vector(divide_vector(subtract_vector(pixel_color, chosen_color), 10), 3);
-        // let color = mult_vector(divide_vector(chosen_color, 10), 3);
 
-        // se perto, então mantém
-        // se longe, move pra perto mas só um pouco
-        // se muito longe, gray
+        let pointing_vector = subtract_vector(chosen_color, gray_color);
 
-        new_img.put_pixel(x, y, add_vector(color, gray));
+        let distance: u8 = std::cmp::min(length(array_to_u32_array(pointing_vector)), 255) as u8;
+
+        let factor: f32 = 441.0/distance as f32;
+
+        println!("{}", factor);
+
+        let addition_color = mult_vector(pointing_vector, factor);
+
+        new_img.put_pixel(x, y, add_vector(gray_color, addition_color));
     }
 
     // Write the contents of this image to the Writer in PNG format.
-    new_img.save("test31.jpg").unwrap();
+    new_img.save("outputs/test35.jpg").unwrap();
 }
